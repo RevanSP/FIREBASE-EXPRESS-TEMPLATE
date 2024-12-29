8c7622b50ae0d06db4cb53124dff280b329e79e6
Revan SP <110446896+RevanSP@users.noreply.github.com>
2024-12-29 18:46:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,9 @@ const { getFirebaseConfig } = require('./public/lib/firebase');
 
 const app = express();
 const port = 1503;
-const storedPassword = process.env.PASSWORD;
 
 app.use(express.static('public'));
 app.use(express.json());
-
-if (storedPassword) {
-  console.log("SUCCESS: .env loaded.");
-} else {
-  console.log("FAILED: .env or PASSWORD missing.");
-}
 
 app.get('/firebase-config', (req, res) => {
   res.json(getFirebaseConfig());
